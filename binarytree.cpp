25f99cb8517ed7a777de153e87eb45940bd72add
Dinesh-Gandhi-Padamatinti <dineshpadamatinti1997@gmail.com>
2020-12-19 14:04:15 +0530

    fourth commit

---
+++ binarytree.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+#include "Queue.h"
+using namespace std;
+class binary_tree
+{
+	public:
+		treenode *root;
+		binary_tree()
+		{
+			root=NULL;
+		}
+		insert_element(int ele) // level-order insertion
+		{
+			
+		}
+};
+int main()
+{
+	
+}
